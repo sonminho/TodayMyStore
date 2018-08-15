379316c79fa85a098cd228bfe972486579527728
sonminho <30867443+sonminho@users.noreply.github.com>
2018-08-16 03:35:39 +0900

    서버 주석 추가

--- TestServer/TodayMyStore/src/action/AndroidIdCheck.java
+++ TestServer/TodayMyStore/src/action/AndroidIdCheck.java
@@ -17,11 +17,11 @@ public class AndroidIdCheck implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Gson gson = new Gson();
 		
-		UserDTO user = gson.fromJson(request.getParameter("user"), UserDTO.class);
+		UserDTO user = gson.fromJson(request.getParameter("user"), UserDTO.class); // 클라이언트로부터 JSON 객체를 받음
 		System.out.println(user.getmId() + " 계정 확인 중");
 		
 		UserDAO uDao = new UserDAO();
-		int result = uDao.userIdCheck(user.getmId());
+		int result = uDao.userIdCheck(user.getmId()); // 사용자 아이디를 검색 후 테이블에서 삭제
 		
 		// 계정 확인 완료
 		if(result == 1){
